3d09d8bf5087d47f0fb8a13ae05d04d7c572afc3
Hong Shin <hongshin@google.com>
2024-12-10 11:44:19 -0800

    Internal change
    PiperOrigin-RevId: 704790929

--- conformance/failure_list_trie_node_test.cc
+++ conformance/failure_list_trie_node_test.cc
@@ -1,10 +1,15 @@
+// Protocol Buffers - Google's data interchange format
+// Copyright 2024 Google LLC.  All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
 #include "failure_list_trie_node.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "absl/status/status.h"
-#include "absl/status/statusor.h"
-#include "absl/strings/str_cat.h"
 #include "absl/types/optional.h"
 
 using ::testing::Eq;
