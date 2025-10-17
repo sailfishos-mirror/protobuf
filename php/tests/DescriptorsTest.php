sailfishos-mirror/protobuf

commit b27dd18676054c3dcb5cbcff39e5c2c581feaaeb

Exposed more functions in FieldDescriptor and OneofDescriptor.

php/src/Google/Protobuf/FieldDescriptor.php
@@ -39,6 +39,7 @@ class FieldDescriptor
 {
     use GetPublicDescriptorTrait;
 
+    /** @var  \Google\Protobuf\Internal\FieldDescriptor $internal_desc */
     private $internal_desc;
 
     /**
@@ -82,6 +83,22 @@ class FieldDescriptor
     }
 
     /**
+     * @return int
+     */
+    public function getOneofIndex()
+    {
+        return $this->internal_desc->getOneofIndex();
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getProto3Optional()
+    {
+        return $this->internal_desc->getProto3Optional();
+    }
+
+    /**
      * @return Descriptor Returns a descriptor for the field type if the field type is a message, otherwise throws \Exception
      * @throws \Exception
      */
@@ -113,13 +130,5 @@ class FieldDescriptor
     public function isMap()
     {
         return $this->internal_desc->isMap();
-    }
-
-    /**
-     * @return boolean
-     */
-    public function hasOptionalKeyword()
-    {
-        return $this->internal_desc->hasOptionalKeyword();
     }
 }

php/src/Google/Protobuf/Internal/FieldDescriptor.php
@@ -46,8 +46,8 @@ class FieldDescriptor
     private $message_type;
     private $enum_type;
     private $packed;
-    private $is_map;
     private $oneof_index = -1;
+    private $proto3_optional;
 
     public function __construct()
     {
@@ -169,6 +169,16 @@ class FieldDescriptor
         return $this->packed;
     }
 
+    public function getProto3Optional()
+    {
+        return $this->proto3_optional;
+    }
+
+    public function setProto3Optional($proto3_optional)
+    {
+        $this->proto3_optional = $proto3_optional;
+    }
+
     public function isPackable()
     {
         return $this->isRepeated() && self::isTypePackable($this->type);
@@ -214,6 +224,10 @@ class FieldDescriptor
             $field_type !== GPBType::BYTES);
     }
 
+    /**
+     * @param FieldDescriptorProto $proto
+     * @return FieldDescriptor
+     */
     public static function getFieldDescriptor($proto)
     {
         $type_name = null;
@@ -269,6 +283,7 @@ class FieldDescriptor
         $field->setLabel($proto->getLabel());
         $field->setPacked($packed);
         $field->setOneofIndex($oneof_index);
+        $field->setProto3Optional($proto->getProto3Optional());
 
         // At this time, the message/enum type may have not been added to pool.
         // So we use the type name as place holder and will replace it with the

php/src/Google/Protobuf/Internal/OneofDescriptor.php
@@ -37,6 +37,7 @@ class OneofDescriptor
     use HasPublicDescriptorTrait;
 
     private $name;
+    /** @var \Google\Protobuf\FieldDescriptor[] $fields */
     private $fields;
 
     public function __construct()
@@ -62,6 +63,11 @@ class OneofDescriptor
     public function getFields()
     {
         return $this->fields;
+    }
+
+    public function isSynthetic()
+    {
+        return count($this->fields) === 1 && $this->fields[0]->getProto3Optional();
     }
 
     public static function buildFromProto($oneof_proto, $desc, $index)

php/src/Google/Protobuf/OneofDescriptor.php
@@ -38,6 +38,7 @@ class OneofDescriptor
 {
     use GetPublicDescriptorTrait;
 
+    /** @var  \Google\Protobuf\Internal\OneofDescriptor $internal_desc */
     private $internal_desc;
 
     /**
@@ -75,6 +76,6 @@ class OneofDescriptor
 
     public function isSynthetic()
     {
-      return $this->internal_desc->isSynthetic();
+        return $this->internal_desc->isSynthetic();
     }
 }

commit 81b3ae7df9bbb27cc983a0f542e3f19469e95144

Implement hasPresence helper in PHP, remove broken hasOptionalKeyword.
hasOptionalKeyword has been broken for 3 years since https://github.com/protocolbuffers/protobuf/commit/b27dd18676054c3dcb5cbcff39e5c2c581feaaeb deleted the internal implementation.  hasPresence uses the better semantics we've rolled out in other languages, which will work well under editions.

PiperOrigin-RevId: 820859843

php/ext/google/protobuf/def.c
@@ -501,6 +501,24 @@ PHP_METHOD(FieldDescriptor, getRealContainingOneof) {
 }
 
 /*
+ * FieldDescriptor::hasPresence()
+ *
+ * Returns true if this field tracks presence, ie. does the field
+ * distinguish between "unset" and "present with default value."
+ *
+ * This includes required, optional, and oneof fields. It excludes maps,
+ * repeated fields, and singular proto3 fields without "optional".
+ *
+ * For fields where hasPresence() == true, the return value of
+ * msg.hasField() is semantically meaningful.
+ *
+ */
+PHP_METHOD(FieldDescriptor, hasPresence) {
+  FieldDescriptor* intern = (FieldDescriptor*)Z_OBJ_P(getThis());
+  RETURN_BOOL(upb_FieldDef_HasPresence(intern->fielddef));
+}
+
+/*
  * FieldDescriptor::getMessageType()
  *
  * Returns the Descriptor for this field, which must be a message.
@@ -531,6 +549,7 @@ static zend_function_entry FieldDescriptor_methods[] = {
   PHP_ME(FieldDescriptor, getEnumType, arginfo_void, ZEND_ACC_PUBLIC)
   PHP_ME(FieldDescriptor, getContainingOneof, arginfo_void, ZEND_ACC_PUBLIC)
   PHP_ME(FieldDescriptor, getRealContainingOneof, arginfo_void, ZEND_ACC_PUBLIC)
+  PHP_ME(FieldDescriptor, hasPresence, arginfo_void, ZEND_ACC_PUBLIC)
   PHP_ME(FieldDescriptor, getMessageType, arginfo_void, ZEND_ACC_PUBLIC)
   ZEND_FE_END
 };

php/src/Google/Protobuf/FieldDescriptor.php
@@ -96,11 +96,20 @@ class FieldDescriptor
     }
 
     /**
+     * Returns true if this field tracks presence, ie. does the field
+     * distinguish between "unset" and "present with default value."
+     *
+     * This includes required, optional, and oneof fields. It excludes maps,
+     * repeated fields, and singular proto3 fields without "optional".
+     *
+     * For fields where hasPresence() == true, the return value of
+     * msg.hasField() is semantically meaningful.
+     *
      * @return boolean
      */
-    public function hasOptionalKeyword()
+    public function hasPresence()
     {
-        return $this->internal_desc->hasOptionalKeyword();
+        return $this->internal_desc->hasPresence();
     }
 
     /**

php/src/Google/Protobuf/Internal/FieldDescriptor.php
@@ -164,6 +164,15 @@ class FieldDescriptor
         $this->proto3_optional = $proto3_optional;
     }
 
+    public function hasPresence()
+    {
+        if ($this->isRepeated()) {
+            return false;
+        }
+        return $this->getType() == GPBType::MESSAGE ||
+               !is_null($this->containing_oneof);
+    }
+
     public function getContainingOneof()
     {
         return $this->containing_oneof;

php/tests/DescriptorsTest.php
@@ -146,6 +146,7 @@ class DescriptorsTest extends TestBase
         $this->assertFalse($fieldDesc->isRepeated());
         $this->assertSame(self::GPBTYPE_INT32, $fieldDesc->getType());
         $this->assertFalse($fieldDesc->isMap());
+        $this->assertFalse($fieldDesc->hasPresence());
 
         // Optional enum field
         $fieldDesc = $fieldDescMap[16];
@@ -156,6 +157,7 @@ class DescriptorsTest extends TestBase
         $this->assertSame(self::GPBTYPE_ENUM, $fieldDesc->getType());
         $this->assertInstanceOf('\Google\Protobuf\EnumDescriptor', $fieldDesc->getEnumType());
         $this->assertFalse($fieldDesc->isMap());
+        $this->assertFalse($fieldDesc->hasPresence());
 
         // Optional message field
         $fieldDesc = $fieldDescMap[17];
@@ -166,6 +168,7 @@ class DescriptorsTest extends TestBase
         $this->assertSame(self::GPBTYPE_MESSAGE, $fieldDesc->getType());
         $this->assertInstanceOf('\Google\Protobuf\Descriptor', $fieldDesc->getMessageType());
         $this->assertFalse($fieldDesc->isMap());
+        $this->assertTrue($fieldDesc->hasPresence());
 
         // Repeated int field
         $fieldDesc = $fieldDescMap[31];
@@ -174,6 +177,7 @@ class DescriptorsTest extends TestBase
         $this->assertTrue($fieldDesc->isRepeated());
         $this->assertSame(self::GPBTYPE_INT32, $fieldDesc->getType());
         $this->assertFalse($fieldDesc->isMap());
+        $this->assertFalse($fieldDesc->hasPresence());
 
         // Repeated message field
         $fieldDesc = $fieldDescMap[47];
@@ -184,6 +188,7 @@ class DescriptorsTest extends TestBase
         $this->assertInstanceOf('\Google\Protobuf\Descriptor', $fieldDesc->getMessageType());
         $this->assertFalse($fieldDesc->isMap());
         $this->assertNull($fieldDesc->getContainingOneof());
+        $this->assertFalse($fieldDesc->hasPresence());
 
         // Oneof int field
         // Tested further in testOneofDescriptor()
@@ -195,6 +200,7 @@ class DescriptorsTest extends TestBase
         $this->assertSame(self::GPBTYPE_INT32, $fieldDesc->getType());
         $this->assertFalse($fieldDesc->isMap());
         $this->assertSame($fieldDesc->getContainingOneof(), $fieldDesc->getRealContainingOneof());
+        $this->assertTrue($fieldDesc->hasPresence());
 
         $oneofDesc = $fieldDesc->getContainingOneof();
         $this->assertSame('my_oneof', $oneofDesc->getName());
@@ -210,6 +216,7 @@ class DescriptorsTest extends TestBase
         $this->assertFalse($fieldDesc->isMap());
         $this->assertNull($fieldDesc->getRealContainingOneof());
         $this->assertNotNull($fieldDesc->getContainingOneof());
+        $this->assertTrue($fieldDesc->hasPresence());
 
         // Map int-enum field
         $fieldDesc = $fieldDescMap[71];
@@ -222,6 +229,7 @@ class DescriptorsTest extends TestBase
         $this->assertSame('descriptors.TestDescriptorsMessage.MapInt32EnumEntry', $mapDesc->getFullName());
         $this->assertSame(self::GPBTYPE_INT32, $mapDesc->getField(0)->getType());
         $this->assertSame(self::GPBTYPE_ENUM, $mapDesc->getField(1)->getType());
+        $this->assertFalse($fieldDesc->hasPresence());
     }
 
     public function testFieldDescriptorEnumException()