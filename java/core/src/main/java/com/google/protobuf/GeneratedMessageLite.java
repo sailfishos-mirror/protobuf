55bc468d3d7c1cbadc3ce49b4c80f159557c8b4b
Mark Hansen <markhansen@google.com>
2026-02-24 21:47:13 -0800

    Tag GeneratedMessageLite.getMemoizedHashCode as final
    This is effectively-final. Tagging it as final will help optimizers make
    devirtualization decisions.
    
    PiperOrigin-RevId: 874947874

--- java/core/src/main/java/com/google/protobuf/GeneratedMessageLite.java
+++ java/core/src/main/java/com/google/protobuf/GeneratedMessageLite.java
@@ -66,15 +66,15 @@ public abstract class GeneratedMessageLite<
   /** For use by generated code only. Lazily initialized to reduce allocations. */
   protected UnknownFieldSetLite unknownFields = UnknownFieldSetLite.getDefaultInstance();
 
-  boolean isMutable() {
+  final boolean isMutable() {
     return (memoizedSerializedSize & MUTABLE_FLAG_MASK) != 0;
   }
 
-  void markImmutable() {
+  final void markImmutable() {
     memoizedSerializedSize &= ~MUTABLE_FLAG_MASK;
   }
 
-  int getMemoizedHashCode() {
+  final int getMemoizedHashCode() {
     return memoizedHashCode;
   }
 
